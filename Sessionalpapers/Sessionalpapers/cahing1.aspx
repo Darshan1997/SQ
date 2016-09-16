7a4024f584a1690a7023a026651d186ca00cefb2
Darshan1997 <gosai.darshan88@gmail.com>
2016-09-16 12:38:42 +0530

    first try

---
+++ Sessionalpapers/Sessionalpapers/cahing1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cahing1.aspx.cs" Inherits="Sessionalpapers.cahing1" %>
+<%@ OutputCache Duration="60" VaryByParam="None" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Sessionalpapers/Sessionalpapers/cahing1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sessionalpapers
+{
+    public partial class cahing1 : System.Web.UI.Page
+    {
+        GridView gv;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //String id1, name1, branch1, sem1;
+            String connstring = "Data Source=CELAB5;Initial Catalog=student;Integrated Security=SSPI";
+            SqlConnection conn = new SqlConnection(connstring);
+            conn.Open();
+           String select = "select * from std_info";
+            SqlCommand command = new SqlCommand(select,conn);
+            
+
+            gv.DataSource = command.ExecuteReader();
+            gv.DataBind();
+
+            Cache.Insert("data", gv);
+            GridView1.DataSource=(GridView) Cache["data"];
+            GridView1.DataBind();
+
+          
+            
+        }
+    }
+}
